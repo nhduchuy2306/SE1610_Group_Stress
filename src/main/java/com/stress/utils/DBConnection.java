83f4b5a0a54b4def14f34c9cef07c464da883716
quangtm0404 <quangtm0152@gmail.com>
2022-09-20 12:25:05 +0700

    addDBConnection(2 )

--- src/main/java/com/stress/utils/DBConnection.java
+++ src/main/java/com/stress/utils/DBConnection.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.stress.util;
+package com.stress.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,18 +18,10 @@ public class DBConnection {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
          Connection conn = null;
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String url = "jdbc:sqlserver://localhost:1433;databaseName=ETransportationManagement";
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=ETransportationManagement;trustServerCertificate=true;";
         conn = DriverManager.getConnection(url, "sa", "12345");
         return conn;
     }
     
-    public static void main(String[] args) {
-        try {
-            System.out.println(DBConnection.getConnection());
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+    
 }
