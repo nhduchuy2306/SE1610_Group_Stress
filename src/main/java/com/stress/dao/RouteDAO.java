c48feab3eee013d1ee3175bb07e14880f5023f3e
unknown <nguyenhuy1323614@gmail.com>
2022-09-22 00:18:04 +0700

    new function

--- src/main/java/com/stress/dao/RouteDAO.java
+++ src/main/java/com/stress/dao/RouteDAO.java
@@ -3,9 +3,12 @@ package com.stress.dao;
 
 import com.stress.dto.Route;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public interface RouteDAO {
     boolean addRoute(Route route) throws SQLException;
     boolean updateRoute(Route route) throws SQLException;
+    List<Route> searchServiceByStartLocation(String startLocation) throws SQLException;
+    List<Route> searchServiceByEndLocation(String endLocation) throws SQLException;
 }
