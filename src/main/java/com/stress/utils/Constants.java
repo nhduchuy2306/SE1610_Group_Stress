5fe0e74528a3a8e55341e27e1b8bd0d0605c9b94
unknown <nguyenhuy1323614@gmail.com>
2022-09-21 09:29:09 +0700

    some code for login

---
+++ src/main/java/com/stress/utils/Constants.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.stress.utils;
+
+/**
+ *
+ * @author Huy
+ */
+public class Constants {
+
+    public static String CAPTCHA_SITE_KEY = "6LcFThUiAAAAAEZk9isOhp_hFXnYQpQPjQtRdU17";
+    public static String CAPTCHA_SECRET_KEY = "6LcFThUiAAAAAD4Mk080Gff2rY7Oob57GrgA8Ohv";
+    public static String GOOGLE_CLIENT_ID = "352140522561-vpmetjr6bjce1vod9b0cppihhbcgdesh.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "dujlslz823Da_oyjQ1JJtTbX";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/AccessGoogle/login-google";
+    public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
