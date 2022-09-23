9e247ef432fbacf2514479f0fbb34ddd15d799ff
unknown <nguyenhuy1323614@gmail.com>
2022-09-23 14:55:27 +0700

    remove action

--- src/main/java/com/stress/controllers/MoMoResponse.java
+++ src/main/java/com/stress/controllers/MoMoResponse.java
@@ -14,14 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author MinhQuang
+ * @author Huy
  */
-@WebServlet(name = "MainController", urlPatterns = {"/MainController"})
-public class MainController extends HttpServlet {
+@WebServlet(name = "MoMoResponse", urlPatterns = {"/MoMoResponse"})
+public class MoMoResponse extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      *
      * @param request servlet request
      * @param response servlet response
@@ -31,12 +30,17 @@ public class MainController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
-        try {
-            
-        } catch (Exception e) {
-        } finally {
-            
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet MoMoResponse</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet MoMoResponse at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
     }
 
