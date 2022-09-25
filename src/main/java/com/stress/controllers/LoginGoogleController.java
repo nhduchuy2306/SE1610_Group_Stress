0948b7e9746c0b07e58152a0cbe74facc838bf2f
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2022-09-25 20:40:07 +0700

    remove controller

--- src/main/java/com/stress/controllers/LoginGoogleController.java
+++ src/main/java/com/stress/controllers/LoginGoogleController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package com.stress.controllers;
 
 import com.stress.dao.UserDAO;
@@ -19,24 +16,15 @@ import javax.servlet.http.HttpSession;
 import com.stress.dto.User;
 import com.stress.service.UserDAOImpl;
 
-/**
- *
- * @author Huy, Quangtm
- */
 @WebServlet(name = "LoginController", urlPatterns = {"/login"})
 public class LoginGoogleController extends HttpServlet {
 
-
-
-    
     private static final String ERROR = "client/login.jsp";
     private static final String USER_ROLE = "1";
     private static final String ADMIN_ROLE = "2"; 
     private static final String ADMIN = "admin/index.jsp";
     private static final String USER = "client/index.jsp";
     
-
-
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
